4d76c1eeac7598a33d6a8766a889d7315a907ab8
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-12-10 02:56:14 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-	base: "/linear-timecode/dist",
-	plugins: [solidPlugin()],
+  plugins: [solidPlugin()],
 });
